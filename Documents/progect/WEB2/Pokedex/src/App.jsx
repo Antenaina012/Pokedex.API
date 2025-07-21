1e923ffe18bc810deb486ea132775b10e4223d88
Antenaina012 <hei.antenaina.7@gmail.com>
2025-07-21 09:21:15 +0300

    feat: added logo_Navbar

---
+++ Documents/progect/WEB2/Pokedex/src/App.jsx
@@ -0,0 +1,16 @@
+import image from '/src/wp3636965-removebg-preview.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className='Navbar'>
+      <ul>
+        <li>
+          <img src={image} alt="Pokemon" />
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
